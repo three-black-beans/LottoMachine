be5cab222b3f678fa52d0c82579e1eee654d82fe
devPark <88180151+three-black-beans@users.noreply.github.com>
2023-03-08 16:39:17 +0900

    Update LottoMachine.java

--- com/example/LottoMachine.java
+++ com/example/LottoMachine.java
@@ -1,19 +1,19 @@
 package com.example;
 
 /*
-구현해야 할 서비스 구조
+Service Architecture to implementation
 
-1. 1~45까지 써 있는 Ball 을 로또 기계에 넣는다.
-2. 로또 기계에 있는 Ball 을 섞는다.
-3. 섞인 Ball 중에 6개를 꺼낸다.
- */
+1. Put in 45 balls in the lotto machine.
+2. Mix these balls for 10,000 times.
+3. Draw 6 balls.
+*/
 public interface LottoMachine {
-    int MAX_BALL_COUNT = 45; // public static 은 생략 가능
+    int MAX_BALL_COUNT = 45; // 'public static' can be omitted
     int RETURN_BALL_COUNT = 6;
 
-    public void setBalls(Ball[] balls); // Ball[] Ball 이 여러 개를 받겠다. 45개를 받는다. abstract 는 생략 가능
+    public void setBalls(Ball[] balls); // get 45 balls. 'abstract' can be omiited
 
-    public void mix(); // 자기가 가지고 있는 Ball 을 섞는다.
+    public void mix();
 
-    public Ball[] getBalls(); // 6개의 Ball 을 반환한다.
+    public Ball[] getBalls(); // return 6 balls
 }
